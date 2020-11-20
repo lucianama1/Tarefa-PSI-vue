48ca31b6d0922d4489143e7754946e1a3bc68029
LucianaMerege <lucianama@dcc.ufrj.br>
2020-11-20 01:36:56 -0300

    (add)chamada da api

---
+++ app-vue-psi/src/components/Card.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="card">
+    <div class="panel" v-for='info in infos' v-bind:key='info.id' v-bind:info="info">
+      <p>{{info.name}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Card',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      infos: [],
+    }
+  },
+  created(){          //carrega assim que o app for aberto
+      axios.get('https://www.breakingbadapi.com/api/characters')
+      .then(response => {
+        this.infos = (response.data)      
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
